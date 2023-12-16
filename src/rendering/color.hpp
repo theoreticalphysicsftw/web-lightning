9abf0956588a339a1b60267eb578c984a8117af5
Mihail Mladenov <theoreticalphysicsftw@gmail.com>
2023-12-16 20:04:55 +0200

    Added YCbCrA <-> RGBA and YUVA <-> RGBA conversions.

--- src/rendering/color.hpp
+++ src/rendering/color.hpp
@@ -24,6 +24,7 @@
 #pragma once
 
 #include <common/types.hpp>
+#include <common/utilities.hpp>
 #include <algebra/algebra.hpp>
 
 namespace WL
@@ -37,10 +38,24 @@ namespace WL
 		U32 packed;
 		struct
 		{
-			U8 r;
-			U8 g;
-			U8 b;
 			U8 a;
+			U8 b;
+			U8 g;
+			U8 r;
+		};
+		struct
+		{
+			U8 a;
+			U8 cr;
+			U8 cb;
+			U8 y;
+		};
+		struct
+		{
+			U8 a;
+			U8 v;
+			U8 u;
+			U8 y;
 		};
 
 		ColorU32(U32 raw = 0);
@@ -49,7 +64,14 @@ namespace WL
 
 		operator Color4() const;
 	};
+
+	inline auto YCbCrAToRGBA(ColorU32) -> ColorU32;
+	inline auto RGBAToYCbCrA(ColorU32) -> ColorU32;
+	inline auto YUVAToRGBA(ColorU32) -> ColorU32;
+	inline auto RGBAToYUVA(ColorU32) -> ColorU32;
+
 }
+
 
 namespace WL
 {
@@ -74,5 +96,45 @@ namespace WL
 	inline ColorU32::operator Color4() const
 	{
 		return Color4(r / 255.f, g / 255.f, b / 255.f, a / 255.f);
+	}
+
+	inline auto YCbCrAToRGBA(ColorU32 ycbcra)-> ColorU32
+	{
+		ColorU32 rgba;
+		rgba.r = ClampedU8(ycbcra.y + 1.402f * (ycbcra.cr - 128.f));
+		rgba.g = ClampedU8(ycbcra.y - 0.34414f * (ycbcra.cb - 128.f) - 0.71414f * (ycbcra.cr - 128.f));
+		rgba.b = ClampedU8(ycbcra.y + 1.772f * (ycbcra.cb - 128.f));
+		rgba.a = ycbcra.a;
+		return rgba;
+	}
+
+	inline auto RGBAToYCbCrA(ColorU32 rgba) -> ColorU32
+	{
+		ColorU32 ycbcra;
+		ycbcra.y = ClampedU8(0.299f * rgba.r + 0.587f * rgba.g + 0.114f * rgba.b);
+		ycbcra.cr = ClampedU8(128.f - 0.1687f * rgba.r - 0.3313f * rgba.g + 0.5f * rgba.b);
+		ycbcra.cb = ClampedU8(128.f + 0.5f * rgba.r - 0.4187f * rgba.g - 0.0813f * rgba.b);
+		ycbcra.a = rgba.a;
+		return ycbcra;
+	}
+
+	auto YUVAToRGBA(ColorU32 yuva) -> ColorU32
+	{
+		ColorU32 rgba;
+		rgba.r = ClampedU8(yuva.y + 1.1398f * yuva.v);
+		rgba.g = ClampedU8(yuva.y - 0.3947f * yuva.u - 0.5806f * yuva.v);
+		rgba.b = ClampedU8(yuva.y + 2.0321f * yuva.u);
+		rgba.a = yuva.a;
+		return rgba;
+	}
+
+	auto RGBAToYUVA(ColorU32 rgba) -> ColorU32
+	{
+		ColorU32 yuva;
+		yuva.y = ClampedU8(0.299f * rgba.r + 0.587f * rgba.g + 0.114f * rgba.b);
+		yuva.u = ClampedU8(-0.1471f * rgba.r - 0.2889f * rgba.g + 0.436f * rgba.b);
+		yuva.v = ClampedU8(0.615f * rgba.r - 0.515f * rgba.g - 0.1f * rgba.b);
+		yuva.a = rgba.a;
+		return yuva;
 	}
 }
