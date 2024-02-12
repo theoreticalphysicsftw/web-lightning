077ed00739d7af50a83e2bdd12b679ebe78d0bfb
Mihail Mladenov <theoreticalphysicsftw@gmail.com>
2024-02-12 23:43:04 +0200

    Partial work on the particle path traversal by arclength.

--- src/animation/path_traversal.hpp
+++ src/animation/path_traversal.hpp
@@ -24,7 +24,10 @@
 #pragma once
 
 #include <rendering/vector_path.hpp>
+#include <algebra/arc_length.hpp>
 #include <common/error.hpp>
+#include <common/utilities.hpp>
+
 
 namespace WL
 {
@@ -33,6 +36,12 @@ namespace WL
 	{
 		using Scalar = TF;
 		using Path2D = Path2D<Scalar>;
+		using Line = Line<Scalar, 2>;
+		using Quadratic = QuadraticBezier<Scalar, 2>;
+		using Cubic = CubicBezier<Scalar, 2>;
+		using LengthReparametrization = Variant<ArcLengthReparametrization<Line>, ArcLengthReparametrization<Quadratic>, ArcLengthReparametrization<Cubic>>;
+
+		static constexpr U32 maxCurvesPerPath = 16;
 
 		struct ParamValue
 		{
@@ -45,13 +54,14 @@ namespace WL
 
 		auto Update(Scalar dt) -> Span<const ParamValue>;
 
-		U32 curvesCount;
+		Array<Scalar> cumulativeLength;
+		Array<LengthReparametrization> reparametrizations;
 		Scalar particleLength;
 		Scalar speed;
 		Scalar initialDelay;
 		Scalar currentTime;
 		Scalar currentStart;
-		StaticArray<ParamValue, 2> currentState;
+		StaticArray<ParamValue, maxCurvesPerPath> currentState;
 	};
 }
 
@@ -59,12 +69,37 @@ namespace WL
 namespace WL
 {
 	template<typename TF>
-	inline PathTraversal<TF>::PathTraversal(const Path2D& paths, Scalar particleLength, Scalar speed, Scalar initialDelay)
+	inline PathTraversal<TF>::PathTraversal(const Path2D& path, Scalar particleLength, Scalar speed, Scalar initialDelay)
 	{
-		WL_ASSERT(particleLength <= 1.0);
 		this->particleLength = particleLength;
 
-		curvesCount = paths.primitives.size();
+		for (auto& prim : path.primitives)
+		{
+			Visit<V>
+			(
+				[&](const auto& prim) -> V
+				{
+					reparametrizations.emplace_back(prim);
+				},
+				prim
+			);
+			
+			Visit<V>
+			(
+				[&](const auto& prim) -> V
+				{
+					cumulativeLength.push_back(prim.GetMaxArcLength());
+				},
+				reparametrizations.back()
+			);
+			
+		}
+
+		for (auto i = 1; i < cumulativeLength.size(); ++i)
+		{
+			cumulativeLength[i] += cumulativeLength[i - 1];
+		}
+
 		this->speed = speed;
 		this->initialDelay = initialDelay;
 		currentTime = 0;
@@ -83,46 +118,16 @@ namespace WL
 
 		currentStart += dt * speed;
 
-		if (currentStart < 0)
-		{
-			currentState[0].curveIdx = 0;
-			currentState[0].t0 = 0;
-			currentState[0].t1 = particleLength + currentStart;
+		auto totalLength = cumulativeLength.back();
 
-			return Span<ParamValue>(currentState.data(), 1u);
-		}
-
-		Scalar totalParamValue = curvesCount;
-		auto progress = currentStart / totalParamValue;
-		auto startCurveIdx = U32(progress);
-
-		if (startCurveIdx >= curvesCount)
+		if (currentStart >= totalLength)
 		{
 			currentTime = 0;
 			currentStart = -particleLength;
 			return {};
 		}
 
-		auto currentStartParam = Frac(progress);
-
-		currentState[0].curveIdx = startCurveIdx;
-		currentState[0].t0 = currentStartParam;
-		currentState[0].t1 = Min(Scalar(1), currentStartParam + particleLength);
-
-		auto remainder = currentStartParam + particleLength - 1;
-
-		if (remainder > 0 && startCurveIdx < curvesCount - 1)
-		{
-			currentState[1].curveIdx = startCurveIdx + 1;
-			currentState[1].t0 = 0;
-			currentState[1].t1 = remainder;
-
-			return Span<ParamValue>(currentState.data(), 2u);
- 		}
-		else
-		{
-			return Span<ParamValue>(currentState.data(), 1u);
-		}
+		auto startCurveIdx = LowerBound(cumulativeLength, Max(Scalar(0), currentStart));
 
 		return {};
 	}
