bdb3bfd7745fe68661d904c5bbb911072f63c416
Mihail Mladenov <theoreticalphysicsftw@gmail.com>
2024-02-23 19:50:05 +0200

    Change default blending.

--- src/gpu_api/webgl.cpp
+++ src/gpu_api/webgl.cpp
@@ -69,7 +69,7 @@ namespace WL
     auto WebGL::EnablePresentSurfaceTransparency() -> V
     {
         glEnable(GL_BLEND);
-        glBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_SRC_ALPHA, GL_ONE);
+        glBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_SRC_ALPHA, GL_DST_ALPHA);
     }
 
     auto WebGL::EnableSampleCoverage() -> V
